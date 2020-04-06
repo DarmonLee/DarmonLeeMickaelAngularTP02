b9d7fb5e4706265e2e3e1c5ca4e6ce185824885f
DarmonLee <lee-mickael.darmon@outlook.fr>
2020-04-06 18:00:43 +0200

    06/04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angulartp01';
+
+  SelectedItem: any = {}
+
+  ItemArray=[
+    {
+      name:"smudge",
+      description:"smudge the cat",
+      image:'/assets/smudge.png'
+    }
+    ,
+    {
+      name:"drake",
+      description:"drake like/dont like",
+      image:'/assets/drake.jpg'
+    }
+    ,
+    {
+      name:"npc",
+      description:"npc meme",
+      image:'/assets/npc.jpg'
+    }
+    ,
+    {
+      name:"black_scientist",
+      description:"he found somethin'",
+      image:'/assets/black_guy.jpg'
+    }
+  ]
+
+  SelectItem(item){
+    this.SelectedItem.image=item.image
+    this.SelectedItem.name=item.name
+    this.SelectedItem.description=item.description
+  }
+
+
+
 }
